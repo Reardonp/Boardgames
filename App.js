3896e2417fb76f3f0a3342bc8eb37c8a14dff12b
reardopb <Reardopb@mail.uc.edu>
2018-02-14 08:24:01 -0500

    initial commit after breaking everything

---
+++ App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import {StackNavigator} from 'react-navigation';
+import HomeScreen from './screens/HomeScreen';
+
+
+const AppNavigator = StackNavigator({
+  Home: {screen: HomeScreen},
+  //Second: {screen: SecondScreen},
+});
+export default class App extends React.Component {
+  render(){
+    return(
+      <AppNavigator />
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
